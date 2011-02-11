5474004fcbc4d5a0f7bceeca18fbb8568cb7969c
gicoupar <gicoupar@localhost>
2011-02-11 17:05:48 +0000

    Added Getters/Setters to Tile Class

--- src/bgwumpus/Tile.java
+++ src/bgwumpus/Tile.java
@@ -1,7 +1,26 @@
 package bgwumpus;
 
+/**
+ * @author gic4
+ *
+ */
 public abstract class Tile {
 	TileType type;
+
+	/**
+	 * @return returns the enum type of the tile
+	 */
+	public TileType getType() {
+		return type;
+	}
+
+	/**
+	 * @param type sets the enumerated type of the tile
+	 */
+	public void setType(TileType type) {
+		this.type = type;
+	}
+	
 	
 	
 }
