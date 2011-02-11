b89262d2d484313c504cf5c0400d5f9afe4b8d26
socksy <socksy@localhost>
2011-02-11 15:38:33 +0000

    created

---
+++ src/bgwumpus/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package bgwumpus;
+
+/**
+ * @author bjl7
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
