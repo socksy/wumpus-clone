189514fe03e6bb7538f62cc357e0bb3a9157659b
socksy <socksy@localhost>
2011-02-16 20:49:57 +0000

    Cleaned up and added documentation

--- src/bgwumpus/GameLogic.java
+++ src/bgwumpus/GameLogic.java
@@ -1,6 +1,12 @@
 package bgwumpus;
 
 public class GameLogic {
+	
+	/**
+	 * Gets a 2 dimensional array of Entities in order to render on top
+	 * Returns objects directly, gives flexibility to know which object is which - something not provided by just type information
+	 * @return 2D array of Entity objects
+	 */
 	public static Entity[][] getEntityMap()
 	{
 		
