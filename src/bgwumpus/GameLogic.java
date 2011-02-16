fa213fd52ba7fd3cc0c7f18921d78f1047787ac2
socksy <socksy@localhost>
2011-02-16 22:18:50 +0000

    Made things static.

--- src/bgwumpus/GameLogic.java
+++ src/bgwumpus/GameLogic.java
@@ -1,6 +1,10 @@
 package bgwumpus;
 
 public class GameLogic {
+	static int[] PlayerLocation = {0,0};
+	static int[] WumpusLocation = {1,0};
+	static Player player1 = new Player();
+	static Wumpus wumpus1 = new Wumpus();
 	
 	/**
 	 * Gets a 2 dimensional array of Entities in order to render on top
@@ -9,6 +13,18 @@ public class GameLogic {
 	 */
 	public static Entity[][] getEntityMap()
 	{
-		
+		Entity[][] entity_map = new Entity[Map.MAP_DIMENSIONS][Map.MAP_DIMENSIONS];
+		for (int i=0; i<Map.MAP_DIMENSIONS; i++) {
+			for (int j=0; i<Map.MAP_DIMENSIONS; j++) {
+				if (PlayerLocation[0]==j && PlayerLocation[1]==i)
+					entity_map[i][j]=player1;
+				else if (WumpusLocation[0]==j && WumpusLocation[1]==i)
+					entity_map[i][j]=wumpus1;
+				else
+					entity_map[i][j]=null;
+					
+			}
+		}
+		return entity_map;
 	}
 }
