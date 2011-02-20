393afa4210ff59f7305c1e01b59788abd85feed6
gicoupar <gicoupar@localhost>
2011-02-20 01:00:02 +0000

    Added Move Player function and got basic keyboard input moving the player. Also moved loading of images to init() rather than loading each repaint.

--- src/bgwumpus/GUI.java
+++ src/bgwumpus/GUI.java
@@ -23,6 +23,13 @@ public class GUI extends javax.swing.JFrame implements UserInterface {
 
 	UserInputHandler inputHandler = new UserInputHandler(); 
 	addKeyListener(inputHandler);
+	
+	//load images
+	Toolkit t = Toolkit.getDefaultToolkit();
+	img.add(t.getImage("dirt.gif"));
+	img.add(t.getImage("pit.gif"));
+	img.add(t.getImage("treasure.png"));
+	img.add(t.getImage("player.gif"));
 
 	setSize(32*Map.MAP_DIMENSIONS,32*Map.MAP_DIMENSIONS);
 	setResizable(false);
@@ -41,26 +48,23 @@ public class GUI extends javax.swing.JFrame implements UserInterface {
 	
 		g.fillRect(0,0,Map.MAP_DIMENSIONS*32,Map.MAP_DIMENSIONS*32);
 		
-		Toolkit t = Toolkit.getDefaultToolkit();
-		img.add(t.getImage("dirt.gif"));
-		img.add(t.getImage("pit.gif"));
-		img.add(t.getImage("treasure.png"));
-		img.add(t.getImage("player.gif"));
-		
 		for(int i=0; i<Map.MAP_DIMENSIONS; i++){
 			for(int j=0; j<Map.MAP_DIMENSIONS; j++){
 				
+				//draw tiles
 				if(Map.getTypeAt(j,i) == TileType.PIT){
 				g.drawImage(img.get(1), j*32, i*32, 32, 32, null);
 				}
 				else if(Map.getTypeAt(j,i) == TileType.TREASURE){
 				g.drawImage(img.get(2), j*32, i*32, 32, 32, null);
 				}
-				else if(GameLogic.player_location.equals(new Point(j,i))){
-				g.drawImage(img.get(3), j*32, i*32, 32, 32, null); 
-				}
 				else {
 				g.drawImage(img.get(0),j*32,i*32,32,32,null);	
+				}
+				
+				//draw entities on top
+				if(GameLogic.player_location.equals(new Point(j,i))){
+					g.drawImage(img.get(3), j*32, i*32, 32, 32, null); 
 				}
 			}
 		}
