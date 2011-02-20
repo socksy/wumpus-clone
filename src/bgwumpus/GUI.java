10b7d52e1cab478f07d92993b9ddece0608730ea
socksy <socksy@localhost>
2011-02-20 02:45:21 +0000

    fixing more merge conflicts

--- src/bgwumpus/GUI.java
+++ src/bgwumpus/GUI.java
@@ -21,44 +21,41 @@ public class GUI extends javax.swing.JFrame implements UserInterface {
 
 	GUI(){
 
-	UserInputHandler inputHandler = new UserInputHandler(); 
-	addKeyListener(inputHandler);
-	
-	//load images
-	Toolkit t = Toolkit.getDefaultToolkit();
-	img.add(t.getImage("dirt.gif"));
-	img.add(t.getImage("pit.gif"));
-	img.add(t.getImage("treasure.png"));
-	img.add(t.getImage("player.gif"));
+		UserInputHandler inputHandler = new UserInputHandler(); 
+		addKeyListener(inputHandler);
 
-	setSize(32*Map.MAP_DIMENSIONS,32*Map.MAP_DIMENSIONS);
-	setResizable(false);
-	setVisible(true);
+		//load images
+		Toolkit t = Toolkit.getDefaultToolkit();
+		img.add(t.getImage("dirt.gif"));
+		img.add(t.getImage("pit.gif"));
+		img.add(t.getImage("treasure.png"));
+		img.add(t.getImage("player.gif"));
 
-	
+		setSize(32*Map.MAP_DIMENSIONS,32*Map.MAP_DIMENSIONS);
+		setResizable(false);
+		setVisible(true);
+
+
 	}
-	
+
 	public void render(){
-		
-	repaint();
+
+		repaint();
 
 	}
-	
+
 	public void paint(Graphics g){
-	
+
 		g.fillRect(0,0,Map.MAP_DIMENSIONS*32,Map.MAP_DIMENSIONS*32);
-		
+
 		for(int i=0; i<Map.MAP_DIMENSIONS; i++){
 			for(int j=0; j<Map.MAP_DIMENSIONS; j++){
-				
-				//draw tiles
-				if(Map.getTypeAt(j,i) == TileType.PIT){
-					g.drawImage(img.get(1), j*32, i*32, 32, 32, null);
+
 				//assume the tile has not been discovered
 				boolean tileIsVisible = false;
-				
+
 				for(int k=0; k<GameLogic.history.size(); k++){
-					
+
 					if(GameLogic.history.get(k).x == j && GameLogic.history.get(k).y == i){
 						tileIsVisible = true;
 					}
@@ -67,32 +64,32 @@ public class GUI extends javax.swing.JFrame implements UserInterface {
 				if(tileIsVisible){
 					//draw tiles
 					if(Map.getTypeAt(j,i) == TileType.PIT){
-					g.drawImage(img.get(1), j*32, i*32, 32, 32, null);
+						g.drawImage(img.get(1), j*32, i*32, 32, 32, null);
 					} else if(Map.getTypeAt(j,i) == TileType.TREASURE){
-					g.drawImage(img.get(2), j*32, i*32, 32, 32, null);
+						g.drawImage(img.get(2), j*32, i*32, 32, 32, null);
 					} else {
-					g.drawImage(img.get(0),j*32,i*32,32,32,null);	
+						g.drawImage(img.get(0),j*32,i*32,32,32,null);	
 					}
-					
+
 				}
 				else {
-					
-					
+
+
 				}
-					
-					//draw entities on top
-					if(GameLogic.player_location.equals(new Point(j,i))){
-						g.drawImage(img.get(3), j*32, i*32, 32, 32, null); 
-					}
-					
+
+				//draw entities on top
+				if(GameLogic.player_location.equals(new Point(j,i))){
+					g.drawImage(img.get(3), j*32, i*32, 32, 32, null); 
+				}
+
 			}
 		}
-		
+
 		render();
 
 
 	}
-	
-	
-	
+
+
+
 }
