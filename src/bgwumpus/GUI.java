20c7eb931907370577827b5cde5e7bc508cbb341
Gordon Coupar <352696+gicoupar@users.noreply.github.com>
2011-02-22 17:46:11 +0000

    Added in more changes to the User Interface, the status bar is now drawn above the map rather than over it.

--- src/bgwumpus/GUI.java
+++ src/bgwumpus/GUI.java
@@ -5,6 +5,7 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Point;
 import javax.swing.JFrame;
+import javax.swing.JTextArea;
 
 import java.awt.Toolkit;
 import java.util.ArrayList;
@@ -20,6 +21,7 @@ public class GUI extends javax.swing.JFrame implements UserInterface {
 	 */
 	private static final long serialVersionUID = 5111988071128070407L;
 	private DrawingCanvas canvas; 
+	private JTextArea textbox;
 	private boolean messagesUpdated = false;
 	int messageListSize = 0;
 
@@ -30,21 +32,23 @@ public class GUI extends javax.swing.JFrame implements UserInterface {
 		
 		//create a new canvas where all drawing will occur
 		canvas = new DrawingCanvas(player);
+		textbox = new JTextArea();
 
 		//create a new user input handler to listen for keyboard input
 		UserInputHandler inputHandler = new UserInputHandler(player); 
 		addKeyListener(inputHandler); //add it to the frame to use it
 
 		//set the map size to: the number of tiles * the tile size
-		setSize(Map.MAP_DIMENSIONS*canvas.TILE_DIMENSIONS,Map.MAP_DIMENSIONS*canvas.TILE_DIMENSIONS+20);
+		setSize(Map.MAP_DIMENSIONS*canvas.TILE_DIMENSIONS,Map.MAP_DIMENSIONS*canvas.TILE_DIMENSIONS+20+canvas.STATUS_BAR_SIZE);
 		add(canvas); //add the canvas to the frame
+		//add(textbox); //add the textbox to the frame
 
 		//window attributes
 		setTitle("Hunt the Wumpus!"); //set the window bar title
 		setDefaultCloseOperation(EXIT_ON_CLOSE); //enable exit upon closing the window
 		setVisible(true); //show the window
 		setResizable(false); //disable resizing of the window
-
+		
 	}
 
 	/* (non-Javadoc) 
