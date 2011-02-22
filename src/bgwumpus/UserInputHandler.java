ee5e8a272e6b84d74afd29250c17e4315274028c
socksy <socksy@localhost>
2011-02-22 03:35:57 +0000

    Can shoot by holding down shift and pressing an arrow key now.

--- src/bgwumpus/UserInputHandler.java
+++ src/bgwumpus/UserInputHandler.java
@@ -11,6 +11,7 @@ import java.awt.event.KeyListener;
 public class UserInputHandler implements KeyListener {
 	
 	Player player;
+	boolean shift_down = false;
 	
 	UserInputHandler(Player player){
 		
@@ -59,6 +60,8 @@ public class UserInputHandler implements KeyListener {
 			case KeyEvent.VK_DOWN:  break;
 			case KeyEvent.VK_ESCAPE: System.exit(0); break; //quit upon hitting escape
 			case KeyEvent.VK_R: GameLogic.map_revealed = true; break; 
+			case KeyEvent.VK_SHIFT: shift_down = true; break;
+			
 
 		}
 		
@@ -71,11 +74,36 @@ public class UserInputHandler implements KeyListener {
 		switch(key_code){
 		
 			//move up,down,left,or right when corresponding arrow key is released
-			case KeyEvent.VK_RIGHT: player.moveRight();  break;
-			case KeyEvent.VK_LEFT: player.moveLeft(); break;
-			case KeyEvent.VK_UP:  player.moveUp(); break;
-			case KeyEvent.VK_DOWN:  player.moveDown(); break;
+			case KeyEvent.VK_LEFT: 
+				if (!shift_down) {
+					player.moveLeft(); 
+				} else {
+					player.shoot(0);
+				}
+				break;
+			case KeyEvent.VK_UP:  
+				if (!shift_down) {
+					player.moveUp(); 
+				} else {
+					player.shoot(1);
+				}
+				break;
+			case KeyEvent.VK_RIGHT: 
+				if(!shift_down) {
+					player.moveRight();
+				} else {
+					player.shoot(2);
+				}
+				break;
+			case KeyEvent.VK_DOWN:  
+				if (!shift_down) {
+					player.moveDown();
+				} else {
+					player.shoot(3);
+				}
+				break;
 			case KeyEvent.VK_R: GameLogic.map_revealed = false; break; 
+			case KeyEvent.VK_SHIFT: shift_down = false; break;
 
 		}
 		
